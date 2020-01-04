6d6367a1c4d7d9cef31432681bf50c685b223aab
k._lled <saberhusky474@gmail.com>
2020-01-04 11:07:13 +0700

    Clear ALl

--- flutter/dev/ci/docker_linux/docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAG="${CIRRUS_TAG:-latest}"
-
-# Convert "+" to "-" to make hotfix tags legal Docker tag names.
-# See https://docs.docker.com/engine/reference/commandline/tag/
-TAG=${TAG/+/-}
-
-sudo docker push "gcr.io/flutter-cirrus/build-flutter-image:$TAG"
