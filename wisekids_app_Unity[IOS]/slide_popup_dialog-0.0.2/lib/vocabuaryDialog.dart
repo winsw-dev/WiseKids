7bfdda0a3c8ea7faa13ccbb6c2a0b7e9a11ef939
k._lled <saberhusky474@gmail.com>
2020-02-04 19:41:38 +0700

    Test_IOS_AR

---
+++ wisekids_app_Unity[IOS]/slide_popup_dialog-0.0.2/lib/vocabuaryDialog.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+
+class VocabuaryDialog extends StatefulWidget {
+  final Widget child;
+  
+
+  VocabuaryDialog({
+    @required this.child,
+    
+  });
+
+  @override
+  _VocabuaryDialogState createState() => _VocabuaryDialogState();
+}
+
+class _VocabuaryDialogState extends State<VocabuaryDialog> {
+ /*  var _initialPosition = 0.0;
+  var _currentPosition = 0.0; */
+
+  @override
+  Widget build(BuildContext context) {
+    /* final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height; */
+
+    return AnimatedPadding(
+      padding: MediaQuery.of(context).viewInsets +
+          EdgeInsets.only(left: 8,right: 8,top: 19,bottom: 19),
+      duration: Duration(milliseconds: 100),
+      curve: Curves.decelerate,
+      child: MediaQuery.removeViewInsets(
+        removeLeft: true,
+        removeTop: true,
+        removeRight: true,
+        removeBottom: true,
+        context: context,
+        child: widget.child,
+      ),
+    );
+  }
+
+  
+}
